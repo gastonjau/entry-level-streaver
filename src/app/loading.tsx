d4cca5d997bd7bffc94b8270fffdf38acf8f2317
Gaston Jaurena <gassjau@gmail.com>
2025-12-13 10:26:40 -0300

    loading and fixes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,10 @@
 
-export default () => {
+const Loading = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <p className= "min-h-screen text-gray-600 font-medium">Loading...</p>
     </div>
   )
 }
+
+export default Loading;
